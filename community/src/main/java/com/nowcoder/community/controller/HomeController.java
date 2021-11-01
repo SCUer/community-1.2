8df119f1ed75d5c1e83aebc0deb13588dfd4fbbb
zhaoxingmao <470732759@qq.com>
2021-11-01 14:07:19 +0800

    注册、登录功能完成

--- community/src/main/java/com/nowcoder/community/controller/HomeController.java
+++ community/src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -5,12 +5,12 @@ import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,22 +19,23 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
-    @Resource
+    @Autowired
     private DiscussPostService discussPostService;
 
-    @Resource
+    @Autowired
     private UserService userService;
 
-    @RequestMapping(path="/index", method = RequestMethod.GET)
-    public String getIndexPage(Model model, Page page){
-        //方法调用前，SpringMVC自动实例化Model、Page，而且自动把Page注入Model.
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model, Page page) {
+        // 方法调用钱,SpringMVC会自动实例化Model和Page,并将Page注入Model.
+        // 所以,在thymeleaf中可以直接访问Page对象中的数据.
         page.setRows(discussPostService.findDiscussPostRows(0));
         page.setPath("/index");
 
-        List<DiscussPost> list = discussPostService.findDiscussPost(0,page.getOffset(),page.getLimit());
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String, Object>> discussPosts = new ArrayList<>();
-        if(list !=null){
-            for(DiscussPost post: list){
+        if (list != null) {
+            for (DiscussPost post : list) {
                 Map<String, Object> map = new HashMap<>();
                 map.put("post", post);
                 User user = userService.findUserById(post.getUserId());
@@ -45,4 +46,5 @@ public class HomeController {
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
     }
+
 }
--- community/src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ community/src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -2,22 +2,47 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class DiscussPostService {
+
     @Resource
     private DiscussPostMapper discussPostMapper;
 
-    public List<DiscussPost> findDiscussPost(int userId, int offset, int limit){
+    @Resource
+    private SensitiveFilter sensitiveFilter;
+
+    public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
         return discussPostMapper.selectDiscussPosts(userId, offset, limit);
     }
 
-    public int findDiscussPostRows(int userId){
+    public int findDiscussPostRows(int userId) {
         return discussPostMapper.selectDiscussPostRows(userId);
+    }
+
+    public int addDiscussPost(DiscussPost post) {
+        if (post == null) {
+            throw new IllegalArgumentException("参数不能为空!");
+        }
+
+        // 转义HTML标记
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+        // 过滤敏感词
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
+    }
+
+    public DiscussPost findDiscussPostById(int id){
+        return discussPostMapper.selectDiscussPostById(id);
     }
 }
