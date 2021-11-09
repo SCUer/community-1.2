590acb10863ff5aef959835018dbc2eea010681f
zhaoxingmao <470732759@qq.com>
2021-11-09 13:34:06 +0800

    Kafka消息队列实习系统通知

--- community/src/main/java/com/nowcoder/community/controller/MessageController.java
+++ community/src/main/java/com/nowcoder/community/controller/MessageController.java
@@ -155,10 +155,9 @@ public class MessageController implements CommunityConstant {
 
         // 查询评论类通知
         Message message = messageService.findLatestNotice(user.getId(), TOPIC_COMMENT);
+        Map<String, Object> messageVO = new HashMap<>();
+        messageVO.put("message", message);
         if (message != null) {
-            Map<String, Object> messageVO = new HashMap<>();
-            messageVO.put("message", message);
-
             String content = HtmlUtils.htmlUnescape(message.getContent());
             Map<String, Object> data = JSONObject.parseObject(content, HashMap.class);
 
@@ -172,16 +171,15 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findNoticeUnreadCount(user.getId(), TOPIC_COMMENT);
             messageVO.put("unread", unread);
-            model.addAttribute("commentNotice", messageVO);
         }
+        model.addAttribute("commentNotice", messageVO);
 
 
         // 查询点赞类通知
         message = messageService.findLatestNotice(user.getId(), TOPIC_LIKE);
+        messageVO = new HashMap<>();
+        messageVO.put("message", message);
         if (message != null) {
-            Map<String, Object> messageVO = new HashMap<>();
-            messageVO.put("message", message);
-
             String content = HtmlUtils.htmlUnescape(message.getContent());
             Map<String, Object> data = JSONObject.parseObject(content, HashMap.class);
 
@@ -195,16 +193,14 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findNoticeUnreadCount(user.getId(), TOPIC_LIKE);
             messageVO.put("unread", unread);
-            model.addAttribute("likeNotice", messageVO);
         }
+        model.addAttribute("likeNotice", messageVO);
 
         // 查询关注类通知
         message = messageService.findLatestNotice(user.getId(), TOPIC_FOLLOW);
-
+        messageVO = new HashMap<>();
+        messageVO.put("message", message);
         if (message != null) {
-            Map<String, Object> messageVO = new HashMap<>();
-            messageVO.put("message", message);
-
             String content = HtmlUtils.htmlUnescape(message.getContent());
             Map<String, Object> data = JSONObject.parseObject(content, HashMap.class);
 
@@ -217,8 +213,8 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findNoticeUnreadCount(user.getId(), TOPIC_FOLLOW);
             messageVO.put("unread", unread);
-            model.addAttribute("followNotice", messageVO);
         }
+        model.addAttribute("followNotice", messageVO);
 
 
         // 查询未读消息数量
--- community/src/test/java/com/nowcoder/community/KafkaTests.java
+++ community/src/test/java/com/nowcoder/community/KafkaTests.java
@@ -3,52 +3,55 @@ package com.nowcoder.community;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
 public class KafkaTests {
 
-    @Resource
+    @Autowired
     private KafkaProducer kafkaProducer;
 
     @Test
-    public void testKafka(){
+    public void testKafka() {
         kafkaProducer.sendMessage("test", "你好");
         kafkaProducer.sendMessage("test", "在吗");
 
         try {
-            Thread.sleep(1000*10);
+            Thread.sleep(1000 * 10);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
 }
 
 @Component
-class KafkaProducer{
+class KafkaProducer {
 
-    @Resource
+    @Autowired
     private KafkaTemplate kafkaTemplate;
 
-    public void sendMessage(String topic, String content){
+    public void sendMessage(String topic, String content) {
         kafkaTemplate.send(topic, content);
     }
-}
-@Component
-class KafkaConsumer{
 
-    @KafkaListener(topics={"test"})
-    public void handleMessage(ConsumerRecord record){
+}
+
+@Component
+class KafkaConsumer {
+
+    @KafkaListener(topics = {"test"})
+    public void handleMessage(ConsumerRecord record) {
         System.out.println(record.value());
     }
+
+
 }
