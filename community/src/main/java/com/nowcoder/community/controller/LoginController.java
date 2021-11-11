9e2888a035f0db492727857b43569fbc310cde04
zhaoxingmao <470732759@qq.com>
2021-11-11 10:46:50 +0800

    Spring-Security加入

--- community/src/main/java/com/nowcoder/community/controller/LoginController.java
+++ community/src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -160,6 +161,7 @@ public class LoginController implements CommunityConstant {
     @RequestMapping(path="/logout", method=RequestMethod.GET)
     public String logOut(@CookieValue("ticket") String ticket){
         userService.logout(ticket);
+        SecurityContextHolder.clearContext();
         return "redirect:/login";
     }
 
